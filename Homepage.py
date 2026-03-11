68f35eeeceb5693b45e9251a938f90d7b99d6b87
Gurpreetwork9582 <gurpreet.singh@bmmca.internal>
2026-03-11 16:25:58 -0300

    chnages

--- Homepage.py
+++ Homepage.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 
-Main_Page= st.Page("pages/Main.py",title="Main Questions")
+Main_Page= st.Page("pages/Main.py",title="Quiz Play",icon=	"📓")
 #Upload_Page= st.Page("pages/Upload_Questions.py",title="Upload File")
-Manual_Page = st.Page("pages/Manual_Que.py",title="Enter Question Manually")
+Manual_Page = st.Page("pages/Manual_Que.py",title="Build a Quiz",icon="📘")
 
 pg =st.navigation([Main_Page,Manual_Page])
 
